8df607c86473a201e3a9db589546d9dfe2bb037e
REMNA-R-NAIR <32156164+REMNA-R-NAIR@users.noreply.github.com>
2019-04-01 09:42:37 +0000

    finding spectral features of a single audio file

--- spectral_features.py
+++ spectral_features.py
@@ -1,122 +1,42 @@
+#reading an audio file
 
-import wave
+rate,data=wavfile.read('/home/user/test/a0044.wav')
 
+#get dominating frequencies in sliding windows of 200ms
 
-def read_wave(filename):
+step = rate//5 #3200 sampling points every 1/5 sec 
+window_frequencies = []
+for i in range(0,len(data),step):
+    ft = np.fft.fft(data[i:i+step])
+    freqs = np.fft.fftfreq(len(ft)) #fftq tells you the frequencies associated with the coefficients
+    imax = np.argmax(np.abs(ft))
+    freq = freqs[imax]
+    freq_in_hz = abs(freq *rate)
+    window_frequencies.append(freq_in_hz)
+    #noise is seen between 50Hz and 60Hz
+    filtered_frequencies = [f for f in window_frequencies if 20<f<300 and not 46<f<66] 
+print(filtered_frequencies)
+#finding spectral features
+nobs, minmax, mean, variance, skew, kurtosis = stats.describe(filtered_frequencies)
+median    = np.median(filtered_frequencies)
+mode      = stats.mode(filtered_frequencies).mode[0]
+std       = np.std(filtered_frequencies)
+low,peak  = minmax
+q75,q25   = np.percentile(freqs, [75 ,25])
+iqr       = q75 - q25
 
-    fp=wave.open(filename,'r') 
-    nchannels=fp.getnchannels()
-    framerate=fp.getframerate()
-    nframes=fp.getnframes()
-    sampwidth=fp.getsampwidth()
-
-    z_str=fp.readframes(nframes)
-    fp.close()
-
-    dtype_map={1:np.uint8,2:np.uint16}
-    ys=np.frombuffer(z_str,dtype=dtype_map[sampwidth])
-
-    waveObject=Wave(ys,framerate=framerate)
-
-    return waveObject
-
-
-class Wave:
-
-    def __init__(self,ys,ts=None,framerate=None):
-
-        # ys:wave array
-        # ts:array of time
-
-
-        self.ys=np.asanyarray(ys)
-        self.framerate=framerate
-
-        if ts is None:
-            self.ts =np.arange(len(ys))/self.framerate
-        else:
-            self.ts=ts
-
-    def make_spectrum(self):
-
-        n=len(self.ys);
-        d=1/self.framerate;
-
-        hs = np.fft.rfft(self.ys)
-        fs = np.fft.rfftfreq(n, d)
-        return Spectrum(hs,fs,self.framerate)
-    def spec(self):
-
-       # n=len(self.ys);
-        #d=1/self.framerate;
-
-        hs = np.fft.rfft(self.ys)
-        return hs 
-    def freq(self):
-
-        n=len(self.ys);
-        d=1/self.framerate;
-
-        fs = np.fft.rfftfreq(n, d)
-        return fs 
-    def spectral_properties(self,y,f):
-    
-        spec = y
-        freq = f
-        spec = np.abs(spec)
-        amp = spec / spec.sum()
-        mean = (freq * amp).sum()
-        sd = np.sqrt(np.sum(amp * ((freq - mean) ** 2)))
-        amp_cumsum = np.cumsum(amp)
-        median = freq[len(amp_cumsum[amp_cumsum <= 0.5]) + 1]
-        mode = freq[amp.argmax()]
-        Q25 = freq[len(amp_cumsum[amp_cumsum <= 0.25]) + 1]
-        Q75 = freq[len(amp_cumsum[amp_cumsum <= 0.75]) + 1]
-        IQR = Q75 - Q25
-        z = amp - amp.mean()
-        w = amp.std()
-        skew = ((z ** 3).sum() / (len(spec) - 1)) / w ** 3
-        kurt = ((z ** 4).sum() / (len(spec) - 1)) / w ** 4
-        
-        
-        print("mean:",mean)
-        print("sd:",sd)
-        print("median:",median)
-        print("mode:",mode)
-        print("Q25:",Q25)
-        print("Q75:",Q75)
-        print("IQR:",IQR)
-        print("skew:",skew)
-        print("kurt:",kurt)
-
-
-    
-
-
-class Spectrum:
-
-    def __init__(self,hs,fs,framerate):
-
-        # hs : array of amplitudes (real or complex)
-        # fs : array of frequencies
-
-        self.hs=np.asanyarray(hs)
-        self.fs=np.asanyarray(fs)
-        self.framerate=framerate
-
-    @property
-    def amps(self):
-        return np.absolute(self.hs)
-
-    def plot(self, high=None):
-
-       plt.plot(self.fs, self.amps)
-
-data=read_wave('/home/user/project/f.wav') 
-spectrum=data.make_spectrum()
-spec=data.spec()
-fre=data.freq()
-data.spectral_properties(spec,fre)
-spectrum.plot()
-
+print(nobs)
+print(minmax)
+print(mean)
+print(variance)
+print(skew) 
+print(kurtosis)
+print(median)
+print(mode)
+print(std)
+print(low)
+print(peak)
+print(q75)
+print(q25)
+print(iqr)
 
