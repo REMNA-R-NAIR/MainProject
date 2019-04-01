a564fe1895f78f436f2d6455a80358057521b669
REMNA-R-NAIR <32156164+REMNA-R-NAIR@users.noreply.github.com>
2019-04-01 10:16:24 +0000

    applying svm

--- svm.py
+++ svm.py
@@ -1,35 +1,29 @@
-import cv2
-import csv
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn import metrics
 
-#reading the csv fie
+data=pd.read_csv("feature.csv")
+data_new=pd.read_csv("feature.csv",na_values=['?'])
 
-data=pd.read_csv("voice.csv")
-data_new=pd.read_csv("voice.csv",na_values=['?'])
 data_new.dropna(inplace=True)
-predictions=data_new['label']
+predictions=data_new['gender']
 data_new
-features_raw = data_new[['meanfreq','sd','median','Q25','Q75','IQR','skew','kurt','sp.ent','sfm','mode','centroid','meanfun','minfun','maxfun','meandom','mindom','maxdom','dfrange','modindx']]
-
+features_raw = data_new[['nobs','mean','skew','kurtosis','median','mode','std','low','peak','q25','q75','iqr']]
+#test_raw=test_new[['nobs','mean','skew','kurtosis','median','mode','std','low','peak','q25','q75','iqr']]
 from sklearn.model_selection import train_test_split
 
-# male=0 and female =1
-
-predict_class = predictions.apply(lambda x: 0 if x == "male" else 1)
+predict_class = predictions.apply(lambda x: 0 if x == "Male" else 1)
 np.random.seed(1234)
 
-#80% data used for training
-
-X_train, X_test, y_train, y_test = train_test_split(features_raw, predict_class, train_size=0.80, random_state=1)
-
-
+X_train, X_test, y_train, y_test = train_test_split(features_raw, predict_class, train_size=0.50, random_state=1)
 # Show the results of the split
 
 print ("Training set has {} samples.".format(X_train.shape[0]))
 print ("Testing set has {} samples.".format(X_test.shape[0]))
+print(y_train)
 
-#svm training
+
+# Applying SVM 
 
 import sklearn
 from sklearn import svm
@@ -38,10 +32,14 @@ C = 1.0
 svc = svm.SVC(kernel='linear',C=C,gamma=2)
 svc.fit(X_train, y_train)
 
-from sklearn.metrics import fbeta_score
-#print (X_test)
-predictions_test = svc.predict(X_test)
-predictions_test
+
+print ("xtest",X_test)
+print("ytest",y_test)
+predictions_test = svc.predict(X_test.iloc[[2]])
+print("predictions",predictions_test)
+#print("Accuracy is",metrics.accuracy_score(y_test,predictions_test))
+
+
 
 
 
